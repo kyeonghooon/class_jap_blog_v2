b770b750f971d90324375c2044e1ca747a10f2b6
kyeonghooon <gibbon0510@gmail.com>
2024-10-14 10:11:23 +0900

    :recycle: 리팩토링

--- src/main/java/com/tenco/blog_v1/board/BoardController.java
+++ src/main/java/com/tenco/blog_v1/board/BoardController.java
@@ -1,5 +1,6 @@
 package com.tenco.blog_v1.board;
 
+import com.tenco.blog_v1.common.errors.Exception403;
 import com.tenco.blog_v1.user.User;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -21,99 +22,18 @@ public class BoardController {
 
     // 네이티브 쿼리 연습
     private final BoardNativeRepository boardNativeRepository;
-    // JPA, API, JPQL
+    // JPA API , JPQL
     private final BoardRepository boardRepository;
     private final HttpSession session;
 
-    // 특정 게시글 요청 화면
-    @GetMapping("/board/{id}")
-    public String detail(@PathVariable(name = "id") Integer id, HttpServletRequest request) {
-        // JPA API 사용
-        // Board board = boardRepository.findById(id);
-
-        // JPQL FETCH join 사용
-        Board board = boardRepository.findByIdJoinUser(id);
-
-        // 권한 체크
-        User sessionUser = (User) session.getAttribute("sessionUser");
-        if (sessionUser != null && board.getUser().getId().equals(sessionUser.getId())){
-            request.setAttribute("authUser", true);
-        }
-        request.setAttribute("board", board);
-        return "board/detail";
-    }
-
-    @GetMapping("/")
-    public String index(Model model) {
-        // List<Board> boardList = boardNativeRepository.findAll();
-        // 코드 수정
-        List<Board> boardList = boardRepository.findAll();
-        model.addAttribute("boardList", boardList);
-        return "index";
-    }
-
-    // 게시글 작성 화면
-    @GetMapping("/board/save-form")
-    public String saveForm() {
-        return "board/save-form";
-    }
-
-    // 게시글 저장
-    @PostMapping("/board/save")
-    public String save(BoardDTO.SaveDTO reqDTO) {
-        User sessionUser = (User) session.getAttribute("sessionUser");
-        if (sessionUser == null) {
-            return "redirect:/login-form";
-        }
-        // 파라미터가 올바르게 전달 되었는지 확인
-        log.warn("save 실행 : 제목={}, 내용={}", reqDTO.getTitle(), reqDTO.getContent());
-
-        // SaveDTO에서 toEntity 사용해서 BOard 엔티티로 변환하고 인수 값으로 User 정보를 넣었다.
-        boardRepository.save(reqDTO.toEntity(sessionUser));
-        return "redirect:/";
-    }
-
-    // 게시글 삭제
-    // form 태그에서는 GET, POST 방식만 지원
-    @PostMapping("/board/{id}/delete") // form 활용이기 때문에 delete 선언
-    public String delete(@PathVariable(name = "id") Integer id, HttpServletRequest request) {
-        // 유효성, 인증검사
-        // 세션에서 로그인 사용자 정보 가져오기 -> 인증, 인가(권한)
-        User sessionUser = (User) session.getAttribute("sessionUser");
-        if (sessionUser == null) {
-            return "redirect:/login-form";
-        }
-
-        // 권한 체크
-        Board board = boardRepository.findById(id);
-        if (board == null) {
-            return "redirect:/error-404";
-        }
-
-        if (!board.getUser().getId().equals(sessionUser.getId())) {
-            return "redirect:/error-403";
-        }
-        boardRepository.deleteByIdWithAPI(id);
-        return "redirect:/";
-    }
-
-    // 게시글 수정 화면 요청
-    @GetMapping("board/{id}/update-form")
-    public String updateForm(@PathVariable(name = "id") Integer id, HttpServletRequest request) {
-        // 1. 게시글 조회
-        Board board = boardNativeRepository.findById(id);
-        // 2. 요청 속성에 조회한 게시글 속성 및 값 추가
-        request.setAttribute("board", board);
-        // 뷰 리졸브 - 템플릿 반환
-        return "board/update-form";
-    }
-
     // 게시글 수정 요청 기능
-    @PostMapping("board/{id}/update")
-    public String update(@ModelAttribute BoardDTO.UpdateDTO reqDTO, @PathVariable(name = "id") Integer id) {
+    // board/{id}/update
+    @PostMapping("/board/{id}/update")
+    public String update(@PathVariable(name = "id") Integer id, @ModelAttribute BoardDTO.UpdateDTO reqDto) {
 
         // 1. 데이터 바인딩 방식 수정
-        // 2. 인증 검사 - 로그인 여부 판단
+
+        // 2. 인증 검사 - 로그은 여부 판단
         User sessionUser = (User) session.getAttribute("sessionUser");
         if (sessionUser == null) {
             return "redirect:/login-form";
@@ -121,22 +41,119 @@ public class BoardController {
         // 3. 권한 체크 - 내 글이 맞니?
         Board board = boardRepository.findById(id);
         if (board == null) {
-            return "redirect:/error-404";
+            return "redirect:/";  // 게시글이 없다면 에러 페이지 추후 수정
         }
-
         if (!board.getUser().getId().equals(sessionUser.getId())) {
-            return "redirect:/error-403";
+            throw new Exception403("게시글을 수정할 권한이 없습니다.");
         }
-        // 4. 유효성 검사
-        if (reqDTO.getTitle() == null || reqDTO.getContent() == null
-                || reqDTO.getTitle().trim().length() == 0 || reqDTO.getContent().trim().length() == 0) {
-            return "redirect:/error-400";
-        }
+        // 4. 유효성 검사 - 생략
+
         // 5. 서비스 측 위임 (직접 구현) - 레파지토리 사용
-        //boardRepository.updateByIdJPQL(id, reqDTO.getTitle(), reqDTO.getContent());
-        boardRepository.updateByIdJPA(id, reqDTO.getTitle(), reqDTO.getContent());
+        boardRepository.updateByIdJPA(id, reqDto.getTitle(), reqDto.getContent());
+
         // 6. 리다이렉트 처리
 
         return "redirect:/board/" + id;
+    }
+
+    // 게시글 수정 화면 요청
+    // board/id/update
+    @GetMapping("/board/{id}/update-form")
+    public String updateForm(@PathVariable(name = "id") Integer id, HttpServletRequest request) {
+        // 1. 게시글 조회
+        Board board = boardRepository.findById(id);
+        // 2. 요청 속성에 조회한 게시글 속성 및 값 추가
+        request.setAttribute("board", board);
+        // 뷰 리졸브 - 템플릿 반환
+        return "board/update-form"; // src/main/resources/templates/board/update-form.mustache
+    }
+
+
+    // 주소설계 - http://localhost:8080/board/10/delete ( form 활용이기 때문에 delete 선언)
+// form 태크에서는 GET, POST 방식만 지원하기 때문이다.
+    @PostMapping("/board/{id}/delete")
+    public String delete(@PathVariable(name = "id") Integer id) {
+        // 유효성, 인증검사
+        // 세션에서 로그인 사용자 정보 가져오기 -> 인증(로그인 여부) , 인가(권한 - 내글?)
+        User sessionUser = (User) session.getAttribute("sessionUser");
+
+        if (sessionUser == null) {
+            return "redirect:/login-form";
+        }
+
+        // 권한 체크 - 조회
+        Board board = boardRepository.findById(id);
+        if (board == null) {
+            return "redirect:/error-404";
+        }
+        if (!board.getUser().getId().equals(sessionUser.getId())) {
+            throw new Exception403("게시글을 삭제할 권한이 없습니다.");
+        }
+
+        // 게시글 삭제
+        boardRepository.deleteById(id);
+        // boardNativeRepository.deleteById(id);
+        return "redirect:/";
+
+    }
+
+
+    // 특정 게시글 요청 화면
+// 주소설계 - http://localhost:8080/board/1
+    @GetMapping("/board/{id}")
+    public String detail(@PathVariable(name = "id") Integer id, HttpServletRequest request) {
+        // JPA API 사용
+        //Board board = boardRepository.findById(id);
+        // JPQL FETCH join 사용
+        Board board = boardRepository.findByIdJoinUser(id);
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        // 현재 로그인한 유저와 == 게시글 작성한 유저가 같아면
+        // isOwner = true, !isOwner = false
+        boolean isOwner = false;
+        if (sessionUser != null) {
+            if (sessionUser.getId().equals(board.getUser().getId())) {
+                isOwner = true;
+            }
+        }
+        request.setAttribute("board", board);
+        request.setAttribute("isOwner", isOwner);
+        return "board/detail";
+    }
+
+
+    @GetMapping("/")
+    public String index(Model model) {
+
+        //List<Board> boardList = boardNativeRepository.findAll();
+        // 코드 수정
+        List<Board> boardList = boardRepository.findAll();
+        model.addAttribute("boardList", boardList);
+        return "index";
+    }
+
+    // 주소설계 - http://localhost:8080/board/save-form
+// 게시글 작성 화면
+    @GetMapping("/board/save-form")
+    public String saveForm() {
+        return "board/save-form";
+    }
+
+    // 게시글 저장
+// 주소설계 - http://localhost:8080/board/save
+    @PostMapping("/board/save")
+    public String save(@ModelAttribute BoardDTO.SaveDTO reqDto) {
+        User sessionUser = (User) session.getAttribute("sessionUser");
+
+        if (sessionUser == null) {
+            return "redirect:/login-form";
+        }
+        // 파라미터가 올바르게 전달 되었는지 확인
+        log.warn("save 실행: 제목={}, 내용={}", reqDto.getTitle(), reqDto.getContent());
+
+        // boardNativeRepository.save(title, content);
+        // SaveDTO 에서 toEntity 사용해서 Board 엔티로 변환하고 인수 값으로 User 정보 정보를 넣었다.
+        // 결국 Board 엔티티로 반환이 된다.
+        boardRepository.save(reqDto.toEntity(sessionUser));
+        return "redirect:/";
     }
 }
--- src/main/java/com/tenco/blog_v1/user/User.java
+++ src/main/java/com/tenco/blog_v1/user/User.java
@@ -1,19 +1,16 @@
 package com.tenco.blog_v1.user;
 
-import com.tenco.blog_v1.board.Board;
 import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 
 import java.sql.Timestamp;
-import java.util.List;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
+@Builder
 @Entity
 @Table(name = "user_tb")
 public class User {
@@ -27,24 +24,11 @@ public class User {
     private String password;
     private String email;
 
-    @Column(nullable = false)
+    // "USER", "ADMIN";
+    @Column(nullable = true)
     private String role;
 
     @CreationTimestamp // 엔티티 생성시 자동으로 현재 시간 입력
     private Timestamp createdAt;
 
-    // 단 방향, 양방향 매핑(mappedBy)
-    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY) // 지연 로딩 설정
-    // @OneToMany(mappedBy = "user", fetch = FetchType.EAGER) // 즉시 로딩 설정
-    private List<Board> boards;
-
-    @Builder
-    public User(Integer id, String username, String password, String email, Timestamp createdAt, List<Board> boards) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.email = email;
-        this.createdAt = createdAt;
-        this.boards = boards;
-    }
 }
